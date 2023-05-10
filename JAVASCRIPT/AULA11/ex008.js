e5103dfbb7dd75612224fd06a2f99682b636abac
shellymk <86502172+shellymk@users.noreply.github.com>
2023-05-10 19:45:58 -0300

    js
    js

---
+++ JAVASCRIPT/AULA11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) //condiçao simples
+{
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO! `)
+}
+
+
+console.log(`Informaçao importante: Diriga sempre usando cintos de segurança.`)
+
---
+++ JAVASCRIPT/AULA11/ex009.js
@@ -0,0 +1,10 @@
+var pais = 'Brasil'
+console.log( `Vivendo em ${pais}`)
+if (pais == 'Brasil')
+{
+    console.log('Voce é Brasileiro(a)!')
+}
+else 
+{
+    console.log ('Voce é Estrangeiro(a)!')
+}
