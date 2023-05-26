ee4a94713f08d85bb6e05b88cdff6d5edfc8fb5a
shellymk <86502172+shellymk@users.noreply.github.com>
2023-05-26 12:02:16 -0300

    aula 13 ou 14 de exercicicos
    exercicios

---
+++ JAVASCRIPT/AULA12/ex013.js
@@ -0,0 +1,39 @@
+var agora  = new Date()
+var diaSen = agora.getDay()
+
+diaSen =  6
+
+switch(diaSen)
+{
+    case 0:
+        console.log('Hoje é domingo, Pe de Cachimboo')
+        break
+
+    case 1:
+        console.log('Hoje é segunda, Sua mae da a bunda.')
+        break
+
+    case 2:
+        console.log('Hoje so é terca mesmo e eu to sem ideias e nada melhor do que so teclar em um codigo sem sentido pra desestrecar ne')
+        break
+        
+    case 3:
+        console.log('hoje é Quarta, vei semana que vem eu faco 26 anos, informaçao aleatoria so pra ter coisa aqui')
+        break
+    
+    case 4:
+        console.log('Hoje é quinta, bem.. to comecando cada um assim porque to sem ideia, mas se bem que eu poderia so usa o loremipsum')
+        break
+
+    case 5:
+        console.log('Hoje finalmente é sexta,dia de descansar assim eu espero.')
+        break
+
+    case 6:
+        console.log('Sabado é amanh, esse eu nao comecei com hoje HAHAHA, Sera que um dia eu realmente vou ser uma boa programadora remunerada mesmo ou tudo isso é em vao, espero que nao.')
+        break
+
+    default:
+        console.log ('[ERRO] Dia Invalido!')
+        break
+}
