6085f35746efa8aa543289bd1d8af1ff458df09c
shellymk <86502172+shellymk@users.noreply.github.com>
2023-09-19 07:17:44 -0300

    .
    .

--- JAVASCRIPT/AULA12 -EX/ex015/script.js
+++ JAVASCRIPT/AULA12 -EX/ex015/script.js
@@ -1,3 +1,16 @@
 function verificador()
     let data = new Date()
-    let ano = data.getFullYear
+    let ano = data.getFullYear()
+    let Fano = document.getElementById('txtano')
+    let res = document.querySelector('div#res')
+
+    if(Fano.value.lenght == 0 || Fano.value > ano)
+    {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }
+    else
+    {
+        let fsex = document.getElementsByName('radsex')
+        let idade = ano - (Fano.value)
+        res.innerHTML = `Idade calculada: ${idade}`
+    }
