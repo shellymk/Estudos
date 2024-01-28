5e1332c499e845ebedd27597fa6db13383115573
shellymk <shellymk@users.noreply.github.com>
2024-01-28 16:51:55 -0300

    .....
    .....

--- JAVASCRIPT/AULA12 -EX/ex015/script.js
+++ JAVASCRIPT/AULA12 -EX/ex015/script.js
@@ -2,68 +2,85 @@ function verificar()
 {   
     let data = new Date()
     let ano = data.getFullYear()
-    let Fano = document.getElementById('txtano')
+    let fano = document.getElementById('txtano')
     let res = document.querySelector('div#res')
 
-    if (Fano.value.lenght == 0 || Number(Fano.value) > ano)
+    if (fano.value.lenght == 0 || Number(fano.value) > ano)
     {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     }
     else
     {
         let fsex = document.getElementsByName('radsex')
-        let idade = ano - Number(Fano.value)
+        let idade = ano - Number(fano.value)
         let genero = ''
+
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
 
-            if (fsex[1].checked)
+            if (fsex[0].checked)
             {
                 genero = 'Masculino'  
                     if (idade >= 0 && idade < 10)
                     {
-                    'criança'
-                    img.setAttribute('src', 'criancamenino300.png')
+                        img.setAttribute('src', 'criancamenino300.png')
+                        res.innerHTML = `Criança do sexo ${genero} com ${idade} anos.`
+
                     }
                     else if (idade < 21)
                     {
-                        'jovem'
                         img.setAttribute('src', 'jovemhomem300.1.png')
+                        res.innerHTML = `Jovem do sexo ${genero} com ${idade} anos.`
+
                     }
                     else if(idade < 50)
-                    {
-                        'adulto'
+                    {   
                         img.setAttribute('src', 'adultohomem300.png')
+                        res.innerHTML = `Adulto do sexo ${genero} com ${idade} anos.`
+
                     }
                     else 
-                    {
-                            'idoso'
-                            idosohomem300.png
+                    { 
+                            img.setAttribute('src', 'idosohomem300.png')
+                            res.innerHTML = `Idoso do sexo ${genero} com ${idade} anos.`
+
                     }
                 
             }
-                else if (fsex[0].checked)
+                else if (fsex[1].checked)
                 {
                     genero = 'Feminino'
                         if (idade >= 0 && idade < 10)
                         {
-                        'criança'
+                            
+                            img.setAttribute('src', 'criancamenina300.png')
+                            res.innerHTML = `Criança do sexo ${genero} com ${idade} anos.`
+
                         }
                         else if (idade < 21)
-                        {
-                            'jovem'
+                        {    
+                            img.setAttribute('src', 'jovemmulher300.png')
+                            res.innerHTML = `Jovem do sexo ${genero} com ${idade} anos.`
                         }
                         else if(idade < 50)
                         {
-                            'adulto'
+                            
+                            img.setAttribute('src', 'adultomulher300.png')
+                            res.innerHTML = `Adulto do sexo ${genero} com ${idade} anos.`
                         }
                         else 
                         {
-                            'idoso'
+                            
+                            img.setAttribute('src', 'idosomulher300.png')
+                            res.innerHTML = `Idoso do sexo ${genero} com ${idade} anos.`
+
                         }
                 }
-                    res.computedStyleMap.textttAlign = 'center'
-                    res.innerHTML = `Pessoa do sexo ${genero} com ${idade} anos.`
+                    res.style.textAlign = 'center'
+
+                    
+
+
                     res.appendChild(img)
                 
 
---
+++ JAVASCRIPT/AULA13/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+while (c < 12)
+{
+    console.log('Tudo bem?')
+    c++ 
+}
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+
+*/ 
