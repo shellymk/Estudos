db76a4133e87cce250187ba0c7bc800a3951ac59
shellymk <sheilamacedo.b@gmail.com>
2024-05-02 15:37:57 -0300

    .
    .

---
+++ JAVASCRIPT/AULA13/part1.js
@@ -0,0 +1,31 @@
+var c = 1
+do
+{
+    console.log(`Tudo bem?? ${c}`)
+    c++
+    
+}while (c <= 3 )
+
+
+
+/*
+var c = 1
+while(c <= 10 )
+{
+    console.log(`Tudo bem?? ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ JAVASCRIPT/AULA14-part2/ambiente.js
@@ -0,0 +1,8 @@
+console.log('Vai comecçar...')
+
+for (var c=1; c <= 5; c++)
+{
+    console.log(c)
+    
+}
+console.log('FIM!')
