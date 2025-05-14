537d5a9f228400e61bda616f3bfaa2e478bbac01
shellymk <sheilamacedo.b@gmail.com>
2025-05-14 18:20:30 -0300

    SEGUINDO MEUS ESTUDOS
    SEGUINDO MEUS ESTUDOS

---
+++ C-estudar/vetor/memoriaeenderecodememoria.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int v[5];
+    int i;
+
+        for (int i = 0; i < 8; i++)
+            {
+            v[i] = i * 10;
+            printf("Insira um dado:\n");
+            scanf("%d", &v[i]);
+            }
+            printf("Dados inseridos: \n");
+            for (i=0; i<5; i++)
+                {
+                    printf("%d\n", v[i]);
+                    printf("v[%d] = %d (endereco: %p)\n", i, v[i], &v[i]);
+
+                }
+}
